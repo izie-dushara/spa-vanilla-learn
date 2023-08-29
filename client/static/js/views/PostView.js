6f0351949f97ffad6fc2fc843fb4b43b3d0e30c9
Izie Zahba <dushara.fahzad62@gmail.com>
2023-08-29 22:04:39 +0800

    Add /:id content

--- client/static/js/views/PostView.js
+++ client/static/js/views/PostView.js
@@ -1,5 +1,19 @@
 import AbstractView from "./AbstractView.js";
 
+const postsData = {
+  1: {
+    title: "Post 1",
+    content: "This is content of post 1",
+  },
+  2: {
+    title: "Post 2",
+    content: "This is content of post 2",
+  },
+  3: {
+    title: "Post 3",
+    content: "This is content of post 3",
+  },
+};
 export default class extends AbstractView {
   constructor(params) {
     super(params);
@@ -8,9 +22,16 @@ export default class extends AbstractView {
   }
 
   async getHtml() {
+    const post = postsData[this.postId];
+    if (post) {
     return `
-    <h1>Posts</h1>
+    <h1>${post.title}</h1>
     <p>You are viewing the post #${this.postId}</p>
+    <p>${post.content}</p>
+    
     `;
+    }else{
+      return `<p> Post Not</p>`
+    }
   }
 }
