ba7720f9c7c5c6c76a845a7ef3a783c462cc65ba
Izie Zahba <dushara.fahzad62@gmail.com>
2023-08-29 17:28:28 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use("/static", express.static(path.resolve(__dirname, "client", "static")));
+app.get("*/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "index.html"));
+});
+
+app.listen(process.env.PORT || 3500, () => {
+  console.log("Server running on port: 3500");
+});
