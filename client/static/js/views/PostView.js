e1fc85bf24aa328af7a3e1cfabf00ddcb086ee6e
Izie Zahba <dushara.fahzad62@gmail.com>
2023-08-29 21:48:32 +0800

    tutorial ended

--- client/static/js/views/PostView.js
+++ client/static/js/views/PostView.js
@@ -3,17 +3,14 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
   constructor(params) {
     super(params);
-    this.setTitle("Posts");
+    this.postId = params.id;
+    this.setTitle("Viewing Post");
   }
 
   async getHtml() {
     return `
     <h1>Posts</h1>
-    <p>You are viewing the posts!</p>
-    <p>
-        <a href="/posts/1" data-link>Post #1</a>
-        <a href="/posts/2" data-link>Post #2</a>
-    </p>
+    <p>You are viewing the post #${this.postId}</p>
     `;
   }
 }
